e4512ae53acee3ed684e291351337a6f0a6a41de
mostafasaberdiab11 <mostafasabermostafa30@gmai.com>
2020-09-02 14:57:55 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="design.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css"/>
+    <title>Post</title>
+  </head>
+  <body>
+    <div class="post-wrapper">
+      <div class="username">
+        <i class="fa fa-user fa-2x"></i>
+        <h1>SFS Yearbook</h1>
+      </div>
+      <div class="post-img">
+        <img src="https://images.unsplash.com/photo-1558980394-4c7c9299fe96?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+      </div>
+      <div class="post-bottom">
+        <div class="post-text">
+          <div class="likes">
+            <h5>12 likes</h5>
+          </div>
+          <div class="comment">
+            <h5>100 comments</h5>
+          </div>
+        </div>
+        <i class="far fa-heart fa-2x" id="like"></i>
+        <i class="far fa-comment fa-2x" id="comment"></i>
+        <i class="far fa-paper-plane fa-2x" id="share"></i>
+        <input type="text" placeholder="Leave a comment"/>
+      </div>
+    </div>
+      <div class="post-wrapper">
+        <div class="username">
+          <i class="fa fa-user fa-2x"></i>
+          <h1>SFS Yearbook</h1>
+        </div>
+        <div class="post-img">
+          <img src="https://images.unsplash.com/photo-1536152470836-b943b246224c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="">
+        </div>
+        <div class="post-bottom">
+          <div class="post-text">
+            <div class="likes">
+              <h5>12 likes</h5>
+            </div>
+            <div class="comment">
+              <h5>100 comments</h5>
+            </div>
+          </div>
+          <i class="far fa-heart fa-2x" id="like"></i>
+          <i class="far fa-comment fa-2x" id="comment"></i>
+          <i class="far fa-paper-plane fa-2x" id="share"></i>
+          <input type="text" placeholder="Leave a comment"/>
+        </div>
+      </div>
+        <div class="post-wrapper">
+          <div class="username">
+            <i class="fa fa-user fa-2x"></i>
+            <h1>SFS Yearbook</h1>
+          </div>
+          <div class="post-img">
+            <img src="https://images.unsplash.com/photo-1558980394-4c7c9299fe96?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+          </div>
+          <div class="post-bottom">
+            <div class="post-text">
+              <div class="likes">
+                <h5>12 likes</h5>
+              </div>
+              <div class="comment">
+                <h5>100 comments</h5>
+              </div>
+            </div>
+            <i class="far fa-heart fa-2x" id="like"></i>
+            <i class="far fa-comment fa-2x" id="comment"></i>
+            <i class="far fa-paper-plane fa-2x" id="share"></i>
+            <input type="text" placeholder="Leave a comment"/>
+          </div>
+        </div>
+          <div class="post-wrapper">
+            <div class="username">
+              <i class="fa fa-user fa-2x"></i>
+              <h1>SFS Yearbook</h1>
+            </div>
+            <div class="post-img">
+              <img src="https://images.unsplash.com/photo-1558980394-4c7c9299fe96?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="u">
+            </div>
+            <div class="post-bottom">
+              <div class="post-text">
+                <div class="likes">
+                  <h5>12 likes</h5>
+                </div>
+                <div class="comment">
+                  <h5>100 comments</h5>
+                </div>
+              </div>
+              <i class="far fa-heart fa-2x" id="like"></i>
+              <i class="far fa-comment fa-2x" id="comment"></i>
+              <i class="far fa-paper-plane fa-2x" id="share"></i>
+              <input type="text" placeholder="Leave a comment"/>
+            </div>
+          </div>
+  </body>
+</html>
